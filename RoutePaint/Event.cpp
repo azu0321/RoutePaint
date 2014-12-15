9e8dbf1a60d0c37bba81f739907db88ba23fee59
Azusa SHIMABUKURO <e125733@ICE.local>
2014-12-15 13:48:42 +0900

    add event fanction

--- RoutePaint/Event.cpp
+++ RoutePaint/Event.cpp
@@ -7,3 +7,27 @@
 //
 
 #include "Event.h"
+
+void Event::event(){
+    
+}
+
+void Event::nomal_road(){
+    
+}
+
+void Event::fire_road(){
+    
+}
+
+void Event::water_road(){
+    
+}
+
+void Event::mushroom_road(){
+    
+}
+
+
+
+
--- RoutePaint/Event.h
+++ RoutePaint/Event.h
@@ -8,6 +8,20 @@
 
 #ifndef RoutePaint_Event_h
 #define RoutePaint_Event_h
+#include "Paint.h"
+
+using namespace std;
+class Event{
+
+    
+public:
+    static void nomal_road();
+    static void fire_road();
+    static void water_road();
+    static void mushroom_road();
+    static void event();
+};
+
 
 
 #endif
